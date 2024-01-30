c3a802a3da5883ddfad1bdbf0927a3d0ace29aa7
tiagoabranges <tiagoabranges@gmail.com>
2024-01-30 16:15:46 -0300

    feat: entity created to product

--- src/produto/produto-caracteristica.entity.ts
+++ src/produto/produto-caracteristica.entity.ts
@@ -1,10 +1,21 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { ProdutoEntity } from './produto.entity';
 
-@Entity('produto_caracteristica')
-export class ProdutoCaracteristica {
+@Entity('produto_caracteristicas')
+export class ProdutoCaracteristicaEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
   @Column({ name: 'nome', length: 100, nullable: false })
   nome: string;
 
-  @Column({ name: 'descricao', length: 100, nullable: false })
+  @Column({ name: 'descricao', length: 255, nullable: false })
   descricao: string;
+
+  @ManyToOne(() => ProdutoEntity, (produto) => produto.imagens, {
+    orphanedRowAction: 'delete',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  produto: ProdutoEntity;
 }
