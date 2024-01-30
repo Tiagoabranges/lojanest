5f056291f134732c1083c7abba38b1002452bb2d
tiagoabranges <tiagoabranges@gmail.com>
2024-01-30 16:18:15 -0300

    feat: usermodule implemented

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioRepository } from './usuario.repository';
 import { EmailEhUnicoValidator } from './validacao/email-eh-unico.validator';
+import { UsuarioService } from './usuario.service';
+import { UsuarioEntity } from './usuario.entity';
+
+//código omitido
+
+//código omitido
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsuarioEntity])],
   controllers: [UsuarioController],
-  providers: [UsuarioRepository, EmailEhUnicoValidator],
+  providers: [UsuarioService, UsuarioRepository, EmailEhUnicoValidator],
 })
 export class UsuarioModule {}
